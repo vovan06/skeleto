d1d132d5a85c78f1adaa4998dc64591e823326bc
vovan06 <barashewova@gmail.com>
2021-09-27 17:34:18 +0300

    фикс

--- venv/app.py
+++ venv/app.py
@@ -61,5 +61,3 @@ if __name__ == "__main__":
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),'favicon.ico',mimetype='image/vnd.microsoft.icon')
 
-app.add_url_rule('/favicon.ico',
-                 redirect_to=url_for('static', filename='favicon.ico'))
